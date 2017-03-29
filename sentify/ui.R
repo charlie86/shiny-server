0d3dd65ca09533bd5ef669cbede07d76b8425319
charlie86 <cthomp870@gmail.com>
2017-03-28 20:58:51 -0400

    fix ui for mobile try

--- sentify/ui.R
+++ sentify/ui.R
@@ -6,9 +6,11 @@ shinyUI(fluidPage(
             textInput('artist_search', 'Type an artist name', value = ''),
             uiOutput('select_artist_ui'),
             uiOutput('album_go_ui'),
-            uiOutput('albums_ui')),
+            uiOutput('albums_ui'),
+            helpText("Click here for more info on Spotify's API")
+        ),
         mainPanel(
-            highchartOutput('quadrant_chart', width = '775px', height='700px')
+            box(highchartOutput('quadrant_chart', width = '775px', height='700px'))
         )
     )
 ))
