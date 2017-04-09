c46e13a17971704ac3a27cdd82c3c86137af18c9
charlie86 <cthomp870@gmail.com>
2017-04-09 02:38:43 -0400

    lowercase user

--- sentify/helpers.R
+++ sentify/helpers.R
@@ -174,6 +174,7 @@ album_feature_chart <- function(df, feature) {
 
 ############ playlists
 get_user_playlists <- function(user) {
+    user <- tolower(user)
     base_url <- 'https://api.spotify.com/v1/'
     user_search_query <- paste0(base_url, 'users/', user, '/playlists')    
     user_playlists <- GET(user_search_query,
